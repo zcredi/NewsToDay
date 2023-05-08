a8abd644f398e12f71e4958cc587d2697b8c1355
zcredi <zcredi@gmail.com>
2023-05-08 16:26:38 +0300

    add Start Project

---
+++ NewsToDay/UIConstants/UIColor+App.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+App.swift
+//  NewsToDay
+//
+//  Created by Владислав on 08.05.2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var purpleTabBar: UIColor {
+        UIColor(red: 0.278, green: 0.353, blue: 0.842, alpha: 1)
+    }
+    
+    static var grayTabBar: UIColor {
+        UIColor(red: 0.674, green: 0.688, blue: 0.766, alpha: 1)
+    }
+}
