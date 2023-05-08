59a53095e2579506d69f622f994efb3cfd253ec3
zcredi <zcredi@gmail.com>
2023-05-08 20:13:04 +0300

    add test

--- NewsToDay/Modules/Category/Controllers/CategoryViewController.swift
+++ NewsToDay/Modules/Category/Controllers/CategoryViewController.swift
@@ -11,7 +11,7 @@ class CategoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+fsdfds
         // Do any additional setup after loading the view.
     }
     
