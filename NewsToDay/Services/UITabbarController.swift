06c533e56fb508aa22952d5426aa8975cca917b1
vikharevam <ViharevAMIP@gmail.com>
2023-05-12 09:59:31 +0300

    Revert "Revert "Feature/bookmarks.table view.extentions""

---
+++ NewsToDay/Services/UITabbarController.swift
@@ -0,0 +1,45 @@
+//
+//  UITabbarController.swift
+//  NewsToDay
+//
+//  Created by Владислав on 08.05.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTabBar()
+        setupItems()
+    }
+    
+    private func setupTabBar() {
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = .purplePrimary
+        tabBar.unselectedItemTintColor = .greyLight
+        tabBar.layer.borderWidth = 0.5
+        tabBar.layer.borderColor = UIColor.greyLight.cgColor
+    }
+    
+    private func setupItems() {
+        
+        let homepage = HomepageViewController()
+        let bookmarks = BookmarksViewController()
+        let profile = ProfileViewController()
+        let category = CategoryViewController()
+        
+
+        setViewControllers([homepage, bookmarks, profile, category], animated: true)
+        
+        guard let items = tabBar.items else { return }
+        
+        items[0].image = UIImage(named: "tabBar_home")
+        items[1].image = UIImage(named: "tabBar_category")
+        items[2].image = UIImage(named: "tabBar_bookmarks")
+        items[3].image = UIImage(named: "tabBar_profile")
+    
+    }
+}
